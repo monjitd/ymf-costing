68c0abd6bca136c385011f72e446ff2080121e77
monjitd <monjit@gmail.com>
2024-10-03 21:40:26 -0400

    Updated prod domain fetching

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,14 +1,18 @@
+import getDomain from "@/app/lib/getDomain"
+
+
 
 async function getData() {
     //1 endpoint - API ?
-    // const endpoint = "http://localhost:3000/api/posts"
-    // const res = await fetch(endpoint)
+    const domain = getDomain()
+    const endpoint = `${domain}/api/posts`
+    const res = await fetch(endpoint)
 
-    // if (!res.ok) {
-    //     throw new Error("Failed to fetch data")
-    // }
-    // return res.json()
-    return {items: []}
+    if (!res.ok) {
+        throw new Error("Failed to fetch data")
+    }
+    return res.json()
+    // return {items: []}
 }
 
 
@@ -16,6 +20,9 @@ async function getData() {
 export default async function BlogPage(){
     const data = await getData()
     const items = data && data.items ? [...data.items] : []
+    console.log(items)
+    console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
+
     return <main>
         <h1>Hello Monjit</h1>
         <h2>Posts:</h2>
