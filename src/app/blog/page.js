e32318f3b883985d6ab20be14e18b9654932d83b
monjitd <monjit@gmail.com>
2024-10-03 22:35:08 -0400

    updated fetch

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -12,7 +12,8 @@ async function getData() {
     //1 endpoint - API ?
     const domain = getDomain()
     const endpoint = `${domain}/api/posts`
-    const res = await fetch(endpoint, {next: {revalidate:10}})
+    const res = await fetch(endpoint, {next: {revalidate: 10 }})
+    // const res = await fetch(endpoint, {cache: 'no-store'})
 
     if (!res.ok) {
         throw new Error("Failed to fetch data")
