3fdb50458516553d6ddaff4b2b6cb398e9ca5bb7
monjitd <monjit@gmail.com>
2024-10-06 22:54:49 -0400

    added

---
+++ src/app/links/page.js
@@ -0,0 +1,9 @@
+import LinksCreateForm from './createForm'
+
+export default function LinksPage() {
+  return (
+      <main className="flex min-h-screen flex-col gap-8 row-start-2 items-center justify-between p-24">
+        <LinksCreateForm />
+      </main>
+  );
+}
